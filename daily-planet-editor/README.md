4f0847bc62bb35126eb35eefc9382be5ed1e73df
Jason Brown <jason.brown91@outlook.com>
2018-11-29 09:07:16 -0700

    Debug Code/ Daily Planet

---
+++ daily-planet-editor/README.md
@@ -0,0 +1,3 @@
+# daily-planet-editor
+
+This is the file associated with the Daily Planet Editor project, which can be found at [coursework.vschool.io/daily-planet-editor/](http://coursework.vschool.io/daily-planet-editor/)
---
+++ daily-planet-editor/paper.js
@@ -0,0 +1,50 @@
+/*
+This code contains only syntax and code style problems. The logic of the code works,
+so DO NOT change the functionality of anything in here.
+
+In short, you shouldn't need to add your own statements anywhere,
+just fix the existing ones.
+*/
+
+
+// Written by Kent, Clark
+
+var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
+
+function whoWins(isThereKryptonite, enemyName) {
+    if (!isThereKryptonite) {
+        return "Superman beats " + enemyName + ", of course";
+    } else {
+        return "Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.";
+    }
+}
+
+for (var i = 0; i < enemies.length; i++) {
+    var isThereKryptonite;
+    if (i % 2 === 0) {
+        isThereKryptonite = true;
+    } else {
+        isThereKryptonite = false;
+    }
+    console.log(whoWins(isThereKryptonite, enemies[i]));
+}
+
+function howAttractedIsLoisLaneToMe() {
+    // 1 is not at all attracted, 10 is "super" attracted...
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+console.log(howAttractedIsLoisLaneToMe());
+
+var clarkKent = true;
+var superman = false;
+
+while (clarkKent) {
+    console.log("I'm just a nerdy columnist");
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+}

de0c7e0f4fb99fba4824c001a97ddf194ebe9739
Jason Brown <jason.brown91@outlook.com>
2018-11-29 15:21:12 -0700

    Update

---
+++ Olympics/app.js
@@ -0,0 +1,76 @@
+//Preliminariy
+
+// if (5 > 3) {
+//     console.log("is greater than")
+// }
+
+// if ("cat".length === 3) {
+//     console.log("is the length")
+// }
+
+// if ("cat" === "dog") {
+
+// } else {
+//     console.log("not the same")
+// }
+
+//Bronze Medal
+
+// var person = {
+//     name: "Bobby",
+//     age: "12"
+// }
+
+// if (person.age >= 18) {
+//     console.log(person.name + " is allowed to go to the movie, because he is 18 or older.")
+// }
+
+// if (person.name.charAt(0) === "B") {
+//     console.log(person.name + " is allowed to go to the movie, because his names starts with a 'B'.")
+// }
+
+// if (person.name.charAt(0) === "B" && person.age >= 18) {
+//     console.log(person.name + " is allowed to go to the movie, because his name starts with a 'B' and he is 18 or older.")
+// }
+
+//Silver Medal
+
+// if (1 === "1"){
+//     console.log("Strict");
+// } else if (1 == "1"){
+//         console.log("loose");
+// } else {
+//     console.log("Doesn't equal at all!");
+// }
+
+// if ((1 <= 2 && 2 === 4) || (3 * 4 > 10 && 5 + 10 > 10)) {
+//     console.log("Yes");
+// }
+
+//Gold Medal
+
+// if (typeof("dog") === "string"){
+//     console.log("'dog' is a string!");
+// }
+
+// if (typeof("true") === "boolean"){
+//     console.log("Yes its a boolean!");
+// } else {
+//     console.log("Hell no its not a boolean.");
+// }
+
+// if (typeof(myVar) === "undefined"){
+//     console.log("No, 'myVar' has not been defined");
+// } else {
+//     console.log("Yes, 'myVar' has been defined.");
+// }
+
+// if ("s" > 1){
+//     console.log("Yes its greater");
+// } else {
+//     console.log("No, its not greater");
+// }
+
+var myNum = 11;
+
+myNum % 2 === 0 ? console.log("The number is Even") : console.log("The number is Odd")

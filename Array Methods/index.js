552845d2728cac59e12615e243e548b5ea18071f
Jason Brown <jason.brown91@outlook.com>
2018-12-03 16:02:59 -0700

    Array Methods

---
+++ Array Methods/index.js
@@ -0,0 +1,18 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+vegetables.pop();
+fruit.shift();
+var index = fruit.indexOf("orange");
+fruit.push(index);
+vegetables.push(vegetables.length);
+var food = [];
+var food = food.concat(fruit, vegetables);
+food.splice(4, 2);
+food = food.reverse();
+food.join(", ") ;
+console.log("Reversed: ", food);
+
+
+
+

eb64bba7755d37c4b07df4ff49e8a06a7e1994e5
Jason Brown <jason.brown91@outlook.com>
2018-12-10 15:53:04 -0700

    Mario Pest Control

---
+++ bugs-on-a-plane/app.js
@@ -0,0 +1,27 @@
+var form = document.getElementById("airline-form");
+var submit = document.getElementById("submit");
+var query = document.querySelector;
+
+function formAlert() {
+    var firstName = form.elements["first-name"].value;
+    var lastName = form.elements["last-name"].value;
+    var age = form.elements["age"].value;
+    var gender = form.elements["gender"].value;
+    var location = form.elements["travel-location"].value;
+    var diet = [];
+    if (form.elements['vegan'].checked) {
+        diet.push(document.getElementById("vegan").value);
+    }
+    if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById('gluten').value);
+    }
+    if (form.elements['paleo'].checked) {
+        diet.push(document.getElementById('paleo').value);
+    }
+
+
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
+
+submit.addEventListener("click", (e) => formAlert());
+

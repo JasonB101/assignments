e9eb81b0ae75e45f8afd03d06e6826299bbf7c58
Ben Turner <bbgrabbag@gmail.com>
2018-12-10 16:32:20 -0700

    feedback

--- mario-pest-control/index.js
+++ mario-pest-control/index.js
@@ -6,6 +6,8 @@ const calculate = document.getElementById("calculate")
 const reset = document.getElementById("reset")
 const amount = document.getElementById("amount")
 
+//Instead of using ternaries, try writing a function that will perform the task, e.g. handleInvalidInput(goomba.value). It'll be less repetitive that way.
+
 calculate.addEventListener("click", (e) => {
     !goomba.value || Math.sign(goomba.valueAsNumber) === -1 ?  goomba.valueAsNumber = 0 : null
     !bobomb.value || Math.sign(bobomb.valueAsNumber) === -1 ?  bobomb.valueAsNumber = 0 : null

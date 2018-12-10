8ec5ae8060f56b328b47028a82414a1aa80b984f
Jason Brown <jason.brown91@outlook.com>
2018-12-10 12:59:58 -0700

    Node Calculator

--- NodeCalc/index.js
+++ NodeCalc/index.js
@@ -1,5 +1,36 @@
 var rs = require('readline-sync')
-console.log(add(+rs.question("Enter a Number:"), +rs.question("Enter a Number:")))
+var options = ["+","*","-","/"]
+
+while(true){
+var num1 = rs.question("Enter number 1: ")
+console.clear()
+console.log(num1)
+var operation = rs.keyInSelect(options, "Choose an operation: ")
+console.clear()
+console.log(num1 + " " + options[operation])
+operation !== -1 ? num2 = rs.question("Enter number 2: ") : null
+console.clear()
+if (operation === 0) {console.log("Answer is: " + add(num1, num2))}
+else if (operation === 1) {console.log(num1 + " " + options[operation] + " " + num2 + " = " + multiply(num1, num2))}
+else if (operation === 2) {console.log(num1 + " " + options[operation] + " " + num2 + " = " + subtract(num1, num2))}
+else if (operation === 3) {console.log(num1 + " " + options[operation] + " " + num2 + " = " + divide(num1, num2))}
+else if (operation === -1) {break}
+}
+
+
 function add(a, b){
     return a + b;
 }
+
+function multiply(a, b){
+    return a * b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
